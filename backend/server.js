f93f8ea8c1ff549a8691aae88cc3d31580a7707e
Anil Acharya <anilacharya974@gmail.com>
2026-06-07 21:20:08 +0545

    Fix CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -53,8 +53,9 @@ const corsOptions = {
   credentials: true,
 };
 
+
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
 const PORT = process.env.PORT || 5000;
 
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
